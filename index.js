8c832277401a545cb441f6ab71cb0e20a504b08e
Edsonjsf <edsonjsf1990@gmail.com>
2023-04-03 13:30:41 -0400

    Configurar servidor de Express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hola desde node");
+const express = require("express");
+
+/* Crear el servidor/aplicación de express */
+const app = express();
+
+/* GET */
+app.get("/", (req, res) => {
+  console.log("Peticion en el /");
+  res.status(200).json({
+    ok: true,
+    msg: "Todo Bien",
+    uid: 1234,
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Servidor corriendo en puerto ${3000}`);
+});
