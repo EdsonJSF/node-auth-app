85045153b360974e6bc209ad55ff01fe61d955c3
Edsonjsf <edsonjsf1990@gmail.com>
2023-04-11 14:24:10 -0400

    Crear usuario en base de datos

--- controlers/auth.js
+++ controlers/auth.js
@@ -1,12 +1,42 @@
 const { response } = require("express");
+const Usuario = require("../models/Usuario");
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
   const { name, mail, pass } = req.body;
 
-  return res.json({
-    ok: true,
-    msg: "Crear usuario",
-  });
+  try {
+    // Verificar el mail
+    const usuario = await Usuario.findOne({ mail });
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Correo electronico existente",
+      });
+    }
+
+    // Crear usuario con el modelo
+    const dbUser = new Usuario(req.body);
+
+    // Hashear pass
+
+    // Generar JWT
+
+    // Crear usuario DB
+    await dbUser.save();
+
+    // Generar respuesta exitosa
+    return res.status(201).json({
+      ok: true,
+      msg: "Usuario creado exitosamente",
+      data: dbUser,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: "Error al crear usuario",
+    });
+  }
 };
 
 const loginUsuario = (req, res = response) => {
