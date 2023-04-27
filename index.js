8d6e514179abf2d9d750981233275f97230def72
Edsonjsf <edsonjsf1990@gmail.com>
2023-04-27 13:51:41 -0400

    Manejo de las demas rutas no declaradas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const { dbConnection } = require("./db/config");
 require("dotenv").config();
 
@@ -20,6 +21,11 @@ app.use(express.json());
 
 /* Rutas */
 app.use("/api/auth", require("./routes/auth"));
+
+/* Manejar demas Rutas */
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
